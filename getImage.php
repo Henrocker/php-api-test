9396b566c8b9b16d4855498ac488800ec2e80a85
Henrik <henrik@neef-privat.de>
2019-01-10 15:01:06 +0100

    Adding files.

---
+++ getImage.php
@@ -0,0 +1,36 @@
+<!DOCTYPE HTML>
+<?php
+$request_method = $_SERVER['REQUEST_METHOD'];
+switch($request_method)
+	{
+		case 'GET':
+			$actual_link = "https://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+//			echo $actual_link;
+			$query_str = parse_url($actual_link, PHP_URL_QUERY);
+			parse_str($query_str, $query_params);
+			if(isset($query_params['image'])) {
+//				echo "Url beinhaltet Parameter \"image\". Nice!<br>";
+//				echo json_encode($query_params);
+//				echo "<br>";
+				$i = $query_params['image'];
+
+				if(!isset($query_params['image']) || !is_numeric($query_params['image'])) {
+					echo 'Nope! Keine Bildnummer angegeben!<br>';
+					break;
+				}
+				if($query_params['image'] > 3 || $query_params['image'] <= 0) {
+					echo 'Nope! Bild No. '.$i.' existiert nicht!<br>';
+					break;
+				}
+
+				echo '<img src="/api/v1/images/'.$i.'.jpg">';
+			}
+			break;
+
+		default:
+			// Nur GET erlaubt!
+			echo "Nope! Try \"GET\" instead.";
+			break;
+	}
+//	echo "<p>Ende!</p>";
+?>
